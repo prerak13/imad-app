58e1da9190f90fb905c74af744d03d2e8278e917
prerak13 <choksiprerak@gmail.com>
2018-03-01 01:14:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ var articles={
     <div> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>`
 }
 
-}
+};
 
 function createTemplate(data){
     var title=data.title;
