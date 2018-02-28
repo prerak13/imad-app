e5372464570d8ff852a9d9aa82f7c71cdf602d10
prerak13 <choksiprerak@gmail.com>
2018-03-01 00:59:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/ui/style.css', function (req, res) {
 }); 
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.send(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
