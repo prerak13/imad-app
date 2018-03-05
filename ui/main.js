e6ac74d6f7d714327b8b4eda472b999145f969c2
prerak13 <choksiprerak@gmail.com>
2018-03-06 01:03:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+var button=document.getElementById('counter');
+var count=0;
+button.onclick=function(){
+    count=count+1;
+    document.getElementById('showThis').innerHTML=count.toString();
+    
+}
