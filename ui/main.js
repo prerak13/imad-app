3e82c95fa0f6a383ac7251e5926042f241d92500
prerak13 <choksiprerak@gmail.com>
2018-03-06 21:33:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,20 @@ console.log('Loaded!');
 var button=document.getElementById('counter');
 var count=0;
 button.onclick=function(){
-    count=count+1;
-    document.getElementById('showThis').innerHTML=count.toString();
     
-}
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readystate==XMLHttpRequest.DONE){
+           if(request.status==200){
+               var count=request.responseText;
+               
+                document.getElementById('showThis').innerHTML=count.toString();
+           } 
+        }
+    };
+    
+    request.open('GET','http://choksiprerak.imad.hasura-app.io/count',true);
+    
+    
+    
+};
